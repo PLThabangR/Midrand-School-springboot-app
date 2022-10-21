d2d380b22d07ee68cb3a3bd4b5dcc7f89627970f
ThabangR <rakgoropothabang@gmail.comgit config --global user.email>
2022-10-21 09:49:48 +0200

    Started a Ivory School web app

---
+++ src/main/java/com/ivoryschool/ivoryschool/IvorySchoolApplication.java
@@ -0,0 +1,13 @@
+package com.ivoryschool.ivoryschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvorySchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvorySchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivoryschool/ivoryschool/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoryschool.ivoryschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvorySchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
