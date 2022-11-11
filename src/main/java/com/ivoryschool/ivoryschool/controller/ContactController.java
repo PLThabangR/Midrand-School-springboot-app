5fb2d1d4b9e98dfdff0a2138742df58771c8787b
ThabangR <rakgoropothabang@gmail.comgit config --global user.email>
2022-11-12 01:11:38 +0200

    Added Spring security and web session for the application

--- src/main/java/com/ivoryschool/ivoryschool/controller/ContactController.java
+++ src/main/java/com/ivoryschool/ivoryschool/controller/ContactController.java
@@ -7,15 +7,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
+import javax.validation.Valid;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
-
+@Slf4j
 @Controller
 public class ContactController {
 
@@ -27,15 +31,21 @@ public class ContactController {
         this.contactService=   contactService;
     }
     @RequestMapping("/contact")
-    public String displayContactPage(){
-
+    public String displayContactPage(Model model){
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
     @RequestMapping(value="/saveMsg",method = POST)
-   public ModelAndView saveMessage(Contact contact){
+   public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors){
+
+        if(errors.hasErrors()){
+            log.error("Contact form validation failed due to :"+errors.toString());
+            return "contact.html";
+        }
+
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return  "redirect:/contact";
     }
 
 }
--- src/main/java/com/ivoryschool/ivoryschool/model/Contact.java
+++ src/main/java/com/ivoryschool/ivoryschool/model/Contact.java
@@ -2,12 +2,27 @@ package com.ivoryschool.ivoryschool.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 public class Contact {
+     @NotBlank(message = "Name must not be blank")
+     @Size(min = 3,message = "Name must be at least 3 characters long")
      private String name;
+     @NotBlank(message = "Email must not be blank")
+     @Email(message = "Provide a valid email")
      private String email;
+     @NotBlank(message = "Subject must not be blank")
+     @Size(min = 3,message = "Subject must be at least 3 characters long")
      private String subject;
+     @NotBlank(message = "Mobile number must not be blank")
+     @Pattern(regexp="^$|[0 - 9]]",message = "Mobile must at least be 10 digits")
      private String mobileNum;
+     @NotBlank(message = "Message must not be blank")
+     @Size(min = 3,message = "Message must be at least 5 characters long")
      private String message;
 
 
